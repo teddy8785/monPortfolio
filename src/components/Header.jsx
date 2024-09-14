37f0039562b1f79a64c905342c1e7be7499b525a
teddy8587 <teddy.barieraud@gmail.com>
2024-09-14 14:30:02 +0200

    feat create react app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/header.css';
+
+function Header() {
+  return (
+      <header className="header">
+      <p>Mon portfolio</p>
+      </header>
+  );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import Header from './components/Header';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+);
