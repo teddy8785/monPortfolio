78c896cbdc6ddd6121ad11974590795d25e4a27b
teddy8587 <teddy.barieraud@gmail.com>
2024-10-15 23:56:20 +0200

    feat changement adresse des images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./styles/index.css";
 import Accueil from "./pages/Accueil.jsx";
 import Projet from "./pages/Projet.jsx";
@@ -9,13 +8,13 @@ import Projet from "./pages/Projet.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/monPortfolio">
       <Routes>
         <Route path="/" element={<Navigate to="/Accueil" />} />
         <Route path="/Accueil" element={<Accueil />} />
         <Route path="/Projet" element={<Projet />} />
         <Route path="/projet/:id" element={<Projet />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
