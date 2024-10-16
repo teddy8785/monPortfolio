266e9bb79b24c519e8be868c7b87e066bec51d05
teddy8587 <teddy.barieraud@gmail.com>
2024-10-16 10:21:11 +0200

    fix ajout hashRouter pour rafraichir correctement la page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./styles/index.css";
 import Accueil from "./pages/Accueil.jsx";
 import Projet from "./pages/Projet.jsx";
@@ -8,13 +8,13 @@ import Projet from "./pages/Projet.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/monPortfolio">
+    <Router basename="/monPortfolio">
       <Routes>
         <Route path="/" element={<Navigate to="/Accueil" />} />
         <Route path="/Accueil" element={<Accueil />} />
         <Route path="/Projet" element={<Projet />} />
         <Route path="/projet/:id" element={<Projet />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
