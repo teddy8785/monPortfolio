9a8990bc8fdc03bca1254e198a0193076f0f1ae8
teddy8785 <teddy.barieraud@gmail.com>
2024-11-05 13:18:41 +0100

    feat affichage page projet en haut de page

--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -4,11 +4,16 @@ import Informations from "../components/Informations";
 import data from "../projets.json";
 import { useParams } from "react-router-dom";
 import "../styles/informations.css";
+import { useEffect } from "react";
 
 function Projet() {
   const { id } = useParams();
   const post = data.find((projet) => projet.id === id);
 
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, []);
+  
   if (!post) {
     return <div>Projet non trouvé</div>;
   }
@@ -19,7 +24,7 @@ function Projet() {
         <h1 className="header__title">{post.nom}</h1>
         <div className="informations__link--content">
           <button
-            className="informations__link"
+            className="informations__return"
             href="#"
             onClick={(e) => {
               e.preventDefault();
