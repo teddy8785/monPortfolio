1062d408875d1a20b4481a337cd7ed293c33b36b
teddy8785 <teddy.barieraud@gmail.com>
2024-11-06 02:25:56 +0100

    feat style conditionnel header page d'accueil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import "../styles/header.css";
+import { useLocation } from "react-router-dom";
 
 function Header({ children }) {
+
+const location = useLocation();
+const isPageAccueil = location.pathname === "/Accueil";
+
+const  accueilStyle = isPageAccueil ? {position:"sticky", top:"0", zIndex:"100"} : {};
+
   return (
-    <header className="header">
+    <header className="header" style={accueilStyle}>
       {children}
     </header>
   );
