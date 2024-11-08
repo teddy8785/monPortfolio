4098591d10981b59f6c47dfa111e200a5cc07a0e
teddy8587 <teddy.barieraud@gmail.com>
2024-11-08 11:55:05 +0100

    feat retrait des entrées dans l'historique

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -4,15 +4,33 @@ import Header from "../components/Header";
 import Presentation from "../components/Presentation";
 import Main from "../components/Main";
 import Contact from "../components/Contact";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 // import Footer from "../components/Footer";
 
 function Accueil() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.replace("#", ""));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+        window.history.replaceState(null, "", window.location.pathname);
+      }
+    }
+  }, [location.hash]);
+
   return (
     <div>
       <Header>
         <h1 className="header__title">PORTFOLIO BARIERAUD</h1>
         <nav className="header__link">
-          <HashLink smooth to="#moi" className="header__link--format header__link--scroll">
+          <HashLink
+            smooth
+            to="#moi"
+            className="header__link--format header__link--scroll"
+          >
             A propos
           </HashLink>
           <HashLink smooth to="#projet" className="header__link--format">
